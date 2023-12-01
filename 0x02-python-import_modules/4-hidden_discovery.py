116654606b0dd2e83a1fac1cae4b4daf06387f68
UkazPromise <ukazpromise@gmail.com>
2023-12-01 00:20:24 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
-import dis
-
-def extract_names():
-    code = open('hidden_4.pyc', 'rb').read()
-    names = set()
-
-    # Disassemble the code and extract names
-    instructions = dis.get_instructions(code)
-    for instruction in instructions:
-        if instruction.opname == 'LOAD_NAME' and not instruction.argrepr.startswith('<'):
-            names.add(instruction.argrepr)
-
-    return sorted(name for name in names if not name.startswith('__'))
-
 if __name__ == "__main__":
-    names = extract_names()
-    for name in names:
-        print(name)
+    from hidden_4 import *
+    arr = dir()
+    for i in range(0, len(arr)):
+        if arr[i][0:2] != "__":
+            print("{}".format(arr[i]))
