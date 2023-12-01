2980597c90040163b8f0a9160cffd1452256ecaf
UkazPromise <ukazpromise@gmail.com>
2023-11-30 23:09:23 -0800

    0-add.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    add_0 = __import__("add_0")  
+    a = 1
+    b = 2
+    result = add_0.add(a, b)
+    print("{} + {} = {}".format(a, b, result))
+
