d42742a02d4b734cd9a9894f4d817036ea8d3ae2
UkazPromise <ukazpromise@gmail.com>
2023-12-04 22:16:49 -0800

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -26,50 +26,51 @@ listint_t *reverse_list(listint_t **head)
 
 /**
  * is_palindrome - checks if a linked list is a palindrome
- * @head: pointer to pointer of first node of listint_t list
- * Return: 0 if not a palindrome, 1 if it is a palindrome
+ * @head: double pointer to the linked list
+ *
+ * Return: 1 if it is, 0 if not
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *slow = *head;
-    listint_t *fast = *head;
-    listint_t *prev_slow = *head;
-    listint_t *second_half;
-    int palindrome = 1;
+	listint_t *slow = *head, *fast = *head, *temp = *head;
+	listint_t *prev = NULL, *second_half = NULL;
 
-    if (*head != NULL && (*head)->next != NULL)
-    {
-        while (fast != NULL && fast->next != NULL)
-        {
-            fast = fast->next->next;
-            prev_slow = slow;
-            slow = slow->next;
-        }
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
 
-        if (fast != NULL)
-        {
-            slow = slow->next;
-        }
+	while (fast && fast->next)
+	{
+		fast = fast->next->next;
+		prev = slow;
+		slow = slow->next;
+	}
+	if (fast)
+	{
+		second_half = slow->next;
+	}
+	else
+	{
+		second_half = slow;
+		prev->next = NULL;
+	}
 
-        second_half = slow;
-        prev_slow->next = NULL;
-        reverse_list(&second_half);
-        listint_t *temp = *head;
+	listint_t *current = *head, *next = NULL;
+	prev = NULL;
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*head = prev;
 
-        while (temp != NULL && second_half != NULL)
-        {
-            if (temp->n != second_half->n)
-            {
-                palindrome = 0;
-                break;
-            }
-            temp = temp->next;
-            second_half = second_half->next;
-        }
-
-        reverse_list(&second_half);
-
-        prev_slow->next = second_half;
-    }
-    return (palindrome);
+	while (second_half && *head)
+	{
+		if (second_half->n != (*head)->n)
+			return (0);
+		second_half = second_half->next;
+		*head = (*head)->next;
+	}
+	return (1);
 }
