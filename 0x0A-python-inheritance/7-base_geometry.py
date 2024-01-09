d7a99a5e92043a1a78f9bf86043b8d94e9b5b280
UkazPromise <ukazpromise@gmail.com>
2024-01-09 15:16:11 -0800

    7-base_geometry.py and 7-base_geometry.txt

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+class BaseGeometry:
+
+
+bg = BaseGeometry()
+
+try:
+    bg.area()
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("my_int", 12)    
+    bg.integer_validator("width", 89) 
+    bg.integer_validator("name", "John") 
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("age", 0)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("distance", -4)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
