0aec40df3e6eda987adde3e3b05b304f5fa68d83
UkazPromise <ukazpromise@gmail.com>
2024-03-18 17:28:30 -0700

    1. Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,46 +1,26 @@
 #!/usr/bin/python3
-
-"""Lists all states from the database hbtn_0e_0_usa, starting with 'N' (uppercase), sorted by ID."""
+"""Lists all states from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
 
-
-def main():
-    """Lists states starting with 'N' from hbtn_0e_0_usa."""
-
-    if len(sys.argv) != 4:
-        print("Usage: ./script_name <username> <password> <database>")
-        return
-
-    username, password, db_name = sys.argv[1:]
-
-    try:
-        # Connect to the MySQL server
-        db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=db_name, port=3306)
-
-        # Create a cursor object
-        cur = db.cursor()
-
-        # Execute the query to filter states by name starting with 'N' and order by id
-        cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
-
-        # Fetch all results
-        rows = cur.fetchall()
-
-        for row in rows:
-            print(row)  # Print each state tuple
-
-    except MySQLdb.Error as err:
-        print(f"Error: {err}")
-
-    finally:
-        # Close cursor and connection
-        if cur:
-            cur.close()
-        if db:
-            db.close()
-
-
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    cur = db.cursor()
+    
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    
+    cur.close()
+    db.close()
